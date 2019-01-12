151008c0b0b862eebf574916a7a2acc742267a1d
Roman Levinzon <levinzon.roman@gmail.com>
2019-01-12 17:20:01 +0100

    Initial Commit

---
+++ StudyPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyPad
+//
+//  Created by Roman Levinzon on 12/01/2019.
+//  Copyright © 2019 Roman Levinzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
