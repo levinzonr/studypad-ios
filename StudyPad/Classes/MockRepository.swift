b540f8eac509718f45ced225e22ca5baa9c240c3
Roman Levinzon <levinzon.roman@gmail.com>
2019-01-12 20:15:16 +0100

    Mock repo updated

--- StudyPad/Classes/MockRepository.swift
+++ StudyPad/Classes/MockRepository.swift
@@ -9,44 +9,61 @@
 import Foundation
 
 final class MockNotebookRepo: NotebookRepository {
+    var items : Array<Notebook> = []
     
-    func getNotebooks(onComplete: @escaping ([Notebook]) -> Void) {
-        var items : Array<Notebook> = []
+    init() {
         for index in 1...20 {
             let color = GradientColor(firstColor: "#fffff", secondColor: "#00000")
             let notebook = Notebook(id: index, name: "Notebook \(index)", notesCount: index, gradientColor: color )
             items.append(notebook)
         }
+        
+    }
+    
+    func getNotebooks(onComplete: @escaping ([Notebook]) -> Void) {
         onComplete(items)
     }
     
     func deleteNotebook(id: Int, onComplete: @escaping () -> Void) {
+        if let index = items.firstIndex(where: { $0.id == id }) {
+            items.remove(at: index)
+        }
         onComplete()
     }
     
     func createNotebook(title: String, onComplete: @escaping (Notebook) -> Void) {
         let color = GradientColor(firstColor: "#fffff", secondColor: "#00000")
-        onComplete(Notebook(id: 231, name: title, notesCount: 0, gradientColor: color))
+        let notebook = Notebook(id: 231, name: title, notesCount: 0, gradientColor: color)
+        items.append(notebook)
+        onComplete(notebook)
     }
 }
 
 final class MockNotesRepository: NotesRepository {
-    
-    func getNotesFromNotebook(notebookId: Int, onComplete: @escaping ([Note]) -> Void) {
-        var items : Array<Note> = []
+    var items : Array<Note> = []
+
+    init() {
         for index in 1...30 {
-            let note = Note(id: index, title: "Title of \(index) note", content: "Content of \(index)th note", notebookId: notebookId)
+            let note = Note(id: index, title: "Title of \(index) note", content: "Content of \(index)th note", notebookId: 1)
             items.append(note)
         }
+        
+    }
+    
+    func getNotesFromNotebook(notebookId: Int, onComplete: @escaping ([Note]) -> Void) {
         onComplete(items)
     }
     
     func deleteNote(noteId: Int, onComplete: @escaping () -> Void) {
+        if let index = items.firstIndex(where:  { $0.id == noteId}) {
+            items.remove(at: index)
+        }
         onComplete()
     }
 
     func createNote(notebookId: Int, title: String, content: String, onComplete: @escaping (Note) -> Void) {
         let note = Note(id: 111, title: title, content: content, notebookId: notebookId)
+        items.append(note)
         onComplete(note)
     }
     
---
+++ StudyPad/Extenstions.swift
@@ -0,0 +1,32 @@
+//
+//  Extenstions.swift
+//  StudyPad
+//
+//  Created by Roman Levinzon on 12/01/2019.
+//  Copyright © 2019 Roman Levinzon. All rights reserved.
+//
+
+import Foundation
+
+
+extension Array where Element: Equatable {
+    
+    @discardableResult mutating func remove(object: Element) -> Bool {
+        if let index = index(of: object) {
+            self.remove(at: index)
+            return true
+        }
+        return false
+    }
+    
+    @discardableResult mutating func remove(where predicate: (Array.Iterator.Element) -> Bool) -> Bool {
+        if let index = self.index(where: { (element) -> Bool in
+            return predicate(element)
+        }) {
+            self.remove(at: index)
+            return true
+        }
+        return false
+    }
+    
+}
